c06fb079caae5535cfa89a52f902020ddec2c996
Omar Faroque (Anik) <faroque.ae@gmail.com>
2019-12-30 18:54:10 -0600

    created a song list

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -4,12 +4,15 @@ import SongList from "./SongList";
 
 class App extends React.Component {
     render() {
-        return (<div>
+        return (<div className="ui container grid">
+            <div className="ui row">
+                <div className={"column eight wide"}>
+                    <SongList/>
+                </div>
+            </div>
            {/* <img src={Profile} width="100%" height="100%" alt={null}/>
             <h1> Hello World</h1>*/}
-           <SongList/>
         </div>);
     }
 }
-
 export default App;
