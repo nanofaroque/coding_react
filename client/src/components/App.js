b1dc97c90f17f3cdbf0927a7d6dab49456e5bf20
Omar Faroque (Anik) <faroque.ae@gmail.com>
2020-01-01 17:17:50 -0600

    adding a basic header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,22 +5,22 @@ import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 function App() {
     return (
-        <div className="App">
+        <div className="ui container">
             <BrowserRouter>
                 <div>
+                    <Header/>
                     <Route path="/" exact component={StreamList}/>
                     <Route path="/streams/new" exact component={StreamCreate}/>
                     <Route path="/streams/edit" exact component={StreamEdit}/>
                     <Route path="/streams/delete" exact component={StreamDelete}/>
                     <Route path="/streams/show" exact component={StreamShow}/>
                 </div>
-
             </BrowserRouter>
         </div>
     );
 }
-
 export default App;
