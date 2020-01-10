30d14bc4406a424db7d9097f1dad650765b4d0ec
Ofaroque <ofaroque@amazon.com>
2020-01-09 23:00:57 -0600

    project sturcture for managing static resources

---
+++ static_res/src/res/strings/app.js
@@ -0,0 +1,10 @@
+const appStrings = {
+        welcome: {
+            heading: 'Welcome',
+            text1: "What you don't know is what you haven't learn",
+            text2: 'Visit my GitHub at https://github.com/onmyway133',
+            button: 'Log in'
+        }
+};
+
+export default appStrings
